004d3749263744e676f8e43e858ac61db7770a99
Shelby Keene <ShelbyEKeene@gmail.com>
2023-08-27 17:20:51 -0500

    make it better

--- dal.js
+++ dal.js
@@ -1,8 +1,26 @@
-const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb+srv://shelby:QHnd4CMyqg5lKJga@cluster1.yccoufm.mongodb.net/?retryWrites=true&w=majority';
-let db            = null;
-//  QHnd4CMyqg5lKJga
+// const MongoClient = require('mongodb').MongoClient;
+// // const url         = 'mongodb+srv://shelby:QHnd4CMyqg5lKJga@cluster1.yccoufm.mongodb.net/?retryWrites=true&w=majority';
+// let db            = null;
+// //  QHnd4CMyqg5lKJga
 
+
+// // Connect to MongoDB Atlas with useUnifiedTopology option
+// MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+//     if (err) {
+//         console.error("Error connecting to MongoDB Atlas:", err);
+//         return;
+//     }
+//     console.log("Connected successfully to MongoDB Atlas");
+
+//     // Connect to your database (replace 'myproject' with your actual database name)
+//     db = client.db('myproject');
+// });
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGODB_URI; // Use the environment variable
+let db = null;
 
 // Connect to MongoDB Atlas with useUnifiedTopology option
 MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
@@ -15,6 +33,8 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, fu
     // Connect to your database (replace 'myproject' with your actual database name)
     db = client.db('myproject');
 });
+
+// ...rest of your code for creating, finding, and updating users
 
 
 // create user account
